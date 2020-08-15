1073dcb07994fca5d77e8f8c1c25e726719afb3a
Mickey <vickeylee.w@gmail.com>
2020-08-15 15:52:37 +1000

    Add animation

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ form.addEventListener('submit', e => {
 
   // show result on page
   scrollTo(0, 0);
-  result.querySelector('span').textContent = `${score}%`;
+
   result.classList.remove('d-none');
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+
+    if (output === score) {
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
 });
